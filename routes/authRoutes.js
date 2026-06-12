5508e5f1c2a0c4cf09df1a329fc8ee82f642daeb
sandeepkumar23f <sandeep21000552@gmail.com>
2026-06-12 11:42:48 +0530

    designed the path for google login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { SignUp, Login } from "../controllers/authController.js";
+import { SignUp, Login, GoogleLogin } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/signup", SignUp);
 router.post("/login", Login);
+router.post("/auth/google", GoogleLogin); 
 
 export default router;
